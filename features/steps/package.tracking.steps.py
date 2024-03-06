1152936d0832321ba2f681a0c989940185ccd8b5
Azmi SAHIN <azmisahin@outlook.com>
2024-03-06 08:41:26 +0000

    ✨: a new feature
    BDD features standart

---
+++ features/steps/package.tracking.steps.py
@@ -0,0 +1,26 @@
+# features/steps/package.steps.py
+
+from behave import given, when, then
+from unittest.mock import patch
+from src.package.tracking import Tracking
+
+
+@given("a Tracking instance is initialized")
+def step_initialize_tracking_instance(context):
+    context.tracking_instance = Tracking()
+
+
+@when("{method} method is called with {message} message")
+def step_call_log_method(context, method, message):
+    with patch("builtins.print"):
+        context.result = getattr(context.tracking_instance, method)(message)
+
+
+@then("it should write {display} message in console")
+def step_verify_log_output(context, display):
+    if context.result is not None:
+        assert (
+            context.result == display
+        ), f"Expected: {display}, Actual: {context.result}"
+    else:
+        print("Warning: Log method returned None.")
