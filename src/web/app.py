425e6f61e9983a9fa35e8fb1077a9d5424757446
Azmi SAHIN <azmisahin@outlook.com>
2024-03-06 08:45:51 +0000

    ♻️: asource package app file

---
+++ src/web/app.py
@@ -0,0 +1,32 @@
+# src/web/app.py
+import os
+from flask import Flask
+
+# Get environment variables
+APP_ENV = os.environ.get("APP_ENV")
+APP_NAME = os.environ.get("APP_NAME")
+
+# Create Flask app instance
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    """API Gateway Home
+
+    Returns:
+        dict: JSON response containing welcome message, application name, and environment.
+    """
+    return {
+        "message": "Welcome to the API Gateway!",
+        "application": APP_NAME,
+        "environment": APP_ENV,
+    }
+
+
+def main():
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
