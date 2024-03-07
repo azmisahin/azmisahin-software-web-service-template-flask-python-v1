e37c07eb583485b0498a45e5efd5968abfe4b1a2
Azmi SAHIN <azmisahin@outlook.com>
2024-03-08 01:45:12 +0300

    ✅: edit test configurations before and after startup

--- features/environment.py
+++ features/environment.py
@@ -1,16 +1,14 @@
 # features/environment.py
-from src.web.app import create_app
 
 
 def before_all(context):
-    # Initialize Flask app and set the context.app variable
-    context.app = create_app()
-    context.client = context.app.test_client()
+    # Initialize logic if needed
+    print("--| Initialize logic")
 
 
 def after_all(context):
     # Teardown logic if needed
-    pass
+    print("--| Teardown logic")
 
 
 # Additional hooks and configurations can be added as needed
