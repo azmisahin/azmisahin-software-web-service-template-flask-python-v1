06079a5b3b2dc204a8492642b9ff7a7e6505094a
Azmi SAHIN <azmisahin@outlook.com>
2024-03-08 21:05:07 +0000

    ✅: adding missing test apptest

--- tests/web/app_test.py
+++ tests/web/app_test.py
@@ -2,12 +2,15 @@
 import unittest
 from src.web.app import create_app
 
+# Ensure that create_app returns the app instance directly
+app, _ = create_app()
+
 
 class AppTest(unittest.TestCase):
 
     def test_home_endpoint(self):
-        tester = create_app().test_client(self)
-        response = tester.get("/")
+        client = app.test_client(self)
+        response = client.get("/")
         data = response.get_json()
 
         self.assertEqual(response.status_code, 200)
