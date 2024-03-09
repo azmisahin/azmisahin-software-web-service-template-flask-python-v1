1bfb4b843cff763b218bf5bdb59934959b8d764a
Azmi SAHIN <azmisahin@outlook.com>
2024-03-09 10:21:16 +0000

    ✅: application global test

--- tests/web/app_test.py
+++ tests/web/app_test.py
@@ -2,21 +2,28 @@
 import unittest
 from src.web.app import create_app
 
-# Ensure that create_app returns the app instance directly
-app, _, _, _ = create_app()
-
 
 class AppTest(unittest.TestCase):
 
+    def setUp(self):
+        # Set up a testing configuration and create a test app
+        self.app, _, _, _ = create_app()
+        self.app.config["TESTING"] = True
+        self.client = self.app.test_client()
+
+    def tearDown(self):
+        pass  # Clean up if needed
+
     def test_home_endpoint(self):
-        client = app.test_client(self)
-        response = client.get("/")
+        # Test the home endpoint
+        response = self.client.get("/")
         data = response.get_json()
 
         self.assertEqual(response.status_code, 200)
         self.assertIn("message", data)
         self.assertIn("application", data)
         self.assertIn("environment", data)
+        self.assertIn("paths", data)
 
 
 if __name__ == "__main__":
